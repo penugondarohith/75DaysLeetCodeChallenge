f30cc1736875f5dc7fc4a2b4311530d960aca442
penugondarohith <penugondarohith2004@gmail.com>
2026-03-28 21:14:54 +0530

    Time: 156 ms (36.6%), Space: 15.4 MB (67.03%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        count = [0] * 26
+        left = 0
+        max_freq = 0
+        result = 0
+        for right in range(len(s)):
+            count[ord(s[right]) - ord('A')] += 1
+            max_freq = max(max_freq, count[ord(s[right]) - ord('A')])
+            while (right - left + 1) - max_freq > k:
+                count[ord(s[left]) - ord('A')] -= 1
+                left += 1
+            result = max(result, right - left + 1)
+        return result
